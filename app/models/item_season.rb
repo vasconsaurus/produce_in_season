4689d3d49f8c702ca227260c0d107eb36a6b54c1
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-05-30 14:17:18 -0300

    updated validations

--- app/models/item_season.rb
+++ app/models/item_season.rb
@@ -1,3 +1,5 @@
 class ItemSeason < ApplicationRecord
   belongs_to :produce_item
+
+  MONTHS = (1..12).to_a.freeze
 end
--- app/models/produce_item.rb
+++ app/models/produce_item.rb
@@ -1,2 +1,7 @@
 class ProduceItem < ApplicationRecord
+  has_many :item_seasons
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :category, presence: true,
+                       inclusion: { in: %w[fruta legume verdura] }
 end
