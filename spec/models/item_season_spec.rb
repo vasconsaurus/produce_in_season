8cab9ce7c6b5dccbc8e8aec05791bfbd438c6500
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-03 15:07:09 -0300

    changed fruta to fruit in RSpec

--- spec/models/item_season_spec.rb
+++ spec/models/item_season_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemSeason, type: :model do
-  let!(:produce_item) { ProduceItem.create(name: 'Banana', category: 'fruta') }
+  let!(:produce_item) { ProduceItem.create(name: 'Banana', category: 'fruit') }
   subject { described_class.new(produce_item_id: produce_item.id, month_index: 1, country_code: 'br', id: 1) }
 
   it 'is valid with valid attributes' do
