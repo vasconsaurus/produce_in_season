7bda2259ea3681400d387a51c8751f85591291ec
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-03 15:03:22 -0300

    changed inclusion validation to enums in the item_season and produce_item models

--- app/models/item_season.rb
+++ app/models/item_season.rb
@@ -3,6 +3,10 @@
 class ItemSeason < ApplicationRecord
   MONTHS = (1..12).to_a.freeze
 
+  COUNTRIES = %w[BR].freeze
+  enum coutry_code: COUNTRIES.index_by(&:to_sym)
+
   belongs_to :produce_item
+
   validates :month_index, :country_code, presence: true
 end
