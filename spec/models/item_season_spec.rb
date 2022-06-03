4d0f046c7b8747a7fe52f05577393dcfa6129905
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-03 13:57:57 -0300

    fixed the ProduceItem create issue

--- spec/models/item_season_spec.rb
+++ spec/models/item_season_spec.rb
@@ -4,13 +4,11 @@ require 'rails_helper'
 
 RSpec.describe ItemSeason, type: :model do
   let!(:produce_item) { ProduceItem.create(name: 'Banana', category: 'fruta') }
-  subject { described_class.new(produce_item_id: 1, month_index: 1, country_code: 'br', id: 1) }
+  subject { described_class.new(produce_item_id: produce_item.id, month_index: 1, country_code: 'br', id: 1) }
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
-
-  it 'belongs to many produce_items'
 
   describe 'Associations' do
     it { should belong_to(:produce_item).without_validating_presence }
