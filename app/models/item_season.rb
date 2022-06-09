12dac874df39adf9016e59f5e53899d25493c892
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-09 16:12:23 -0300

    changed it to use i18 as suggested

--- app/models/item_season.rb
+++ app/models/item_season.rb
@@ -10,32 +10,5 @@ class ItemSeason < ApplicationRecord
 
   validates :month_index, :country_code, presence: true
 
-  def month(month_index)
-    case month
-      when month_index == 1
-        "january"
-      when month_index == 2
-        "february"
-      when month_index == 3
-        "march"
-      when month_index == 4
-        "april"
-      when month_index == 5
-        "may"
-      when month_index == 6
-        "june"
-      when month_index == 7
-        "july"
-      when month_index == 8
-        "august"
-      when month_index == 9
-        "september"
-      when month_index == 10
-        "october"
-      when month_index == 11
-        "november"
-      when month_index == 12
-        "december"
-    end
-  end
+  def month(index) = I18n.t("date.month_names")[index].downcase
 end
