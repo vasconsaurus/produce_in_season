4a23105fc591a35cb708798a481516c0f26a9bb7
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2022-06-13 11:26:20 -0300

    chore: make the seeds for our db
    We made a seed from our csv file, and added database cleaner, since it provides helpful ways for us to clean up database before and after running them. 
    
    There were also some small fixes: rubocop requests, adding slugs instead of whitespaces and adding a line break to the end of the csv file, and removing strong_migrations from the development group in the gemfile (it shouldn't be there)

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,25 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_530_171_132) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_30_171132) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'item_seasons', force: :cascade do |t|
-    t.integer 'month_index', null: false
-    t.string 'country_code', null: false
-    t.bigint 'produce_item_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['produce_item_id'], name: 'index_item_seasons_on_produce_item_id'
+  create_table "item_seasons", force: :cascade do |t|
+    t.integer "month_index", null: false
+    t.string "country_code", null: false
+    t.bigint "produce_item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["produce_item_id"], name: "index_item_seasons_on_produce_item_id"
   end
 
-  create_table 'produce_items', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'category', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "produce_items", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "category", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'item_seasons', 'produce_items'
+  add_foreign_key "item_seasons", "produce_items"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.strategy = :truncation
+
+DatabaseCleaner.clean
+
+CSV.foreach(Rails.root.join('lib/produce.csv'), headers: true) do |row|
+  item = ProduceItem.find_or_create_by!(name: row[0], category: row[1])
+  ItemSeason.create!(
+    country_code: row[2],
+    month_index: row[3],
+    produce_item_id: item.id
+  )
+end
