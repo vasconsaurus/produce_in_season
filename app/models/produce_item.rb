7bcb965e73c8a8c5d265607c185e28cc35c23f7c
Tales Cione <talesj@gmail.com>
2022-06-14 22:46:24 +0200

    add dependent: destroy to ProduceItem#item_seasons
    It is good practice to establish what should be done to entities owned by a
    model via `has_many` or `has_one`. In rails this means defining a value for
    `dependent`.
    
    More on the values can be found here:
    
    belongs_to:
    https://guides.rubyonrails.org/association_basics.html#options-for-belongs-to-dependent
    
    has_one:
    https://guides.rubyonrails.org/association_basics.html#options-for-has-one-dependent
    
    has_n:
    https://guides.rubyonrails.org/association_basics.html#options-for-has-n-dependent

--- app/models/produce_item.rb
+++ app/models/produce_item.rb
@@ -4,7 +4,7 @@ class ProduceItem < ApplicationRecord
   CATEGORIES = %w[fruit vegetable green].freeze
   enum category: CATEGORIES.index_by(&:to_sym)
 
-  has_many :item_seasons
+  has_many :item_seasons, dependent: :destroy
 
   validates :name, presence: true
   validates :category, presence: true
