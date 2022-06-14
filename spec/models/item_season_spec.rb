ee257d15c6a25cc3fdaac62702a4812ab431f065
Tales Cione <talesj@gmail.com>
2022-06-14 22:50:13 +0200

    name item_season_spec subject

--- spec/models/item_season_spec.rb
+++ spec/models/item_season_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe ItemSeason, type: :model do
-  subject { described_class.new(produce_item_id: produce_item.id, month_index: 1, country_code: 'br', id: 1) }
+  subject(:item_season) do
+    described_class.new(produce_item_id: produce_item.id, month_index: 1, country_code: 'br', id: 1)
+  end
 
   let!(:produce_item) { ProduceItem.create(name: 'Banana', category: 'fruit') }
 
   it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+    expect(item_season).to be_valid
   end
 
   describe 'Associations' do
