b2a687f0ad8fb33a10d2a7c3cb610265de79273c
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-15 15:47:20 -0300

    cleaned up rubocop requests

--- spec/requests/item_seasons_spec.rb
+++ spec/requests/item_seasons_spec.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "ItemSeasons", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/item_seasons/index"
+RSpec.describe 'ItemSeasons', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/item_seasons/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/item_seasons/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/item_seasons/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
