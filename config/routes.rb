027618ce34da5ddfc8c7a96cbf4cdcaf5577f45e
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-15 14:50:29 -0300

    use resources in routes so it would be less cluttered

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'item_seasons/index'
-  get 'item_seasons/show'
-  get 'produce_items/index'
-  get 'produce_items/show'
+  resources :item_seasons, only: %i[index show]
+  resources :produce_items, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
