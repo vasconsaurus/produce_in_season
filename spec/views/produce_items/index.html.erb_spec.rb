fd8d8a31209b7ab52c9f5e9df5162b5e55e49375
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-15 18:38:42 -0300

    fix some rubocop requests

--- spec/views/produce_items/index.html.erb_spec.rb
+++ spec/views/produce_items/index.html.erb_spec.rb
@@ -3,10 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'produce_items/index', type: :view do
-  it "displays the given text" do
+  it 'displays the given text' do
+    render plain: 'This is directly rendered'
 
-    render :plain => "This is directly rendered"
-
-    expect(rendered).to match /directly rendered/
+    expect(rendered).to match(/directly rendered/)
   end
 end
