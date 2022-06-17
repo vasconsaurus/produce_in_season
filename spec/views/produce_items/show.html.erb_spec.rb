53ada02de66ea676227e70c39d1d68630bf41429
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-17 11:20:54 -0300

    wrote some tests for produce item show view

--- spec/views/produce_items/show.html.erb_spec.rb
+++ spec/views/produce_items/show.html.erb_spec.rb
@@ -2,6 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe 'produce_items/show.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'produce_items/show', type: :view do
+  it 'display produce item' do
+    assign(:produce_item, ProduceItem.create!(name: 'banana', category: 'fruit'))
+    render
+    expect(rendered).to match(/banana/)
+  end
+
+  it 'displays months' do
+    @produce_item = ProduceItem.create!(name: 'banana', category: 'fruit')
+    assign(:item_seasons, [
+      ItemSeason.create!(produce_item_id: @produce_item.id, month_index: 1, country_code: 'br', id: 1),
+      ItemSeason.create!(produce_item_id: @produce_item.id, month_index: 2, country_code: 'br', id: 2)
+    ])
+    render
+    expect(rendered).to match(/january/)
+    expect(rendered).to match(/february/)
+  end
 end
