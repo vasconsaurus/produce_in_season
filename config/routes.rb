6f193700e4b96e58ec13e0a3046c4c417cd6d3a9
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-22 09:53:34 -0300

    removed item_seasons controllers, views and routes, as we are not using them

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :item_seasons, only: %i[index show]
   resources :produce_items, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
