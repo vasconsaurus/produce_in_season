389b7cd5979d664c85da57a349d70bb6110b8095
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-23 10:25:27 -0300

    changed our tests to rely on css
    Relying on text matching make the tests a bit too fragile (if we change
    content, tests will break). So I added some classes to our views and used
    those in our tests.
    
    For now, I also changed our show to be a table, so it's structure and style
    would follow our index more closely.

--- spec/features/produce_items_spec.rb
+++ spec/features/produce_items_spec.rb
@@ -13,20 +13,22 @@ RSpec.feature 'ProduceItems', type: :feature do
 
   it 'expects produce items index to have a produce' do
     visit(produce_items_path)
-    expect(page).to have_content('carambola')
+    expect(page).to have_css('table tr td.produce-table__name')
+    expect(page).to have_css('table tr td.produce-table__category')
   end
 
   it 'expects link to be visible' do
     visit(produce_items_path)
-    find_link('ver', visible: :all).visible?
+    find_link(class: ['link_action'], visible: :all).visible?
   end
 
   it 'expects link to navigate to show and back to index' do
     visit(produce_items_path)
-    click_link('ver')
-    expect(find('h1')).to have_content('carambola')
-    expect(page).to have_content('janeiro')
-    click_link('voltar')
+    find('table tr td a.link_action').click
+    expect(page).to have_css('.page__title')
+    expect(page).to have_css('.page__subtitle')
+    expect(page).to have_css('table tr td.produce-table__month')
+    find('.link_action').click
     expect(page).to have_current_path(produce_items_path)
   end
 end
