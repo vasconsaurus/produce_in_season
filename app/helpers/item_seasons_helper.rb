a6dc8e6fdd993e566da8a2380b939085006ddbad
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-27 11:58:59 -0300

    first version of the item seasons index view
    First I tried to write a test. I was thinking we would be displaying our
    item seasons, and did the test with that in mind. But, we have many items
    seasons, what we really are displaying are the 12 months.
    
    We take our months from our dictionary, and get our month indexes from there.
    I did a helper method to do that. We will need the indexes to conect with
    our produce items.
    
    I think that is probably the way to go, which means the view test doesn't
    make sense anymore. I'll rethink it. And will make a helper test as well.

--- app/helpers/item_seasons_helper.rb
+++ app/helpers/item_seasons_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ItemSeasonsHelper
+  def month_name_to_index(month_name)
+    I18n.t('date.month_names').find_index(month_name)
+  end
 end
--- spec/views/item_seasons/index.html.erb_spec.rb
+++ spec/views/item_seasons/index.html.erb_spec.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "item_seasons/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'item_seasons/index', type: :view do
+  let!(:produce_item) { ProduceItem.create(name: 'carambola', category: 'fruit') }
+
+  it 'displays item seasons' do
+    ItemSeason.create!(month_index: 1, country_code: 'BR', produce_item_id: produce_item.id)
+    ItemSeason.create!(month_index: 2, country_code: 'BR', produce_item_id: produce_item.id)
+    assign(:item_seasons, ItemSeason.all)
+    render
+    expect(rendered).to match(/janeiro/)
+  end
 end
