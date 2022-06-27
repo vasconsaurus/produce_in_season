c7083ab20097ae7327e78e5ba485dee03760a905
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-06-27 14:36:01 -0300

    first version of item season show
    The item season show view shows the month and a list of produce, it's name,
    category and link to produce. I added the .include per bullet request to use
    eager loading. I was making a N + 1 query.
    
    I also added two actions to navigate to either the month list or produce list.
    The simple back link could be confusing to the user at times.
    
    I still need to work on view and integration tests.

--- spec/views/item_seasons/show.html.erb_spec.rb
+++ spec/views/item_seasons/show.html.erb_spec.rb
@@ -2,6 +2,14 @@
 
 require 'rails_helper'
 
-RSpec.describe 'item_seasons/show.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'item_seasons/show', type: :view do
+  let!(:produce_item) { ProduceItem.create(name: 'carambola', category: 'fruit') }
+
+  before { ItemSeason.create(produce_item_id: produce_item.id, month_index: 1, country_code: 'br') }
+
+  it 'display produce item' do
+    assign(:produce_item, produce_item)
+    render
+    expect(rendered).to match(/carambola/)
+  end
 end
