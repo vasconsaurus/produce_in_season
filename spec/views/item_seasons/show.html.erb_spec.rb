683c6b7582710daa2c44eb62d3eda08ce8eb72e1
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-07-12 10:22:22 -0300

    item_seasons/show spec: assigned variables before tests
    better to make it DRY as it is still pretty readable

--- spec/views/item_seasons/show.html.erb_spec.rb
+++ spec/views/item_seasons/show.html.erb_spec.rb
@@ -6,22 +6,20 @@ RSpec.describe 'item_seasons/show', type: :view do
   let!(:item_season_produces) { ProduceItem.create(name: 'carambola', category: 'fruit') }
   let!(:item_season) { ItemSeason.create(produce_item_id: item_season_produces.id, month_index: 1, country_code: 'br') }
 
-  it 'display month name' do
+  before do
     assign(:item_season, item_season)
     assign(
       :item_season_produces,
       ProduceItem.joins(:item_seasons).where(item_seasons: { month_index: item_season.month_index })
     )
+  end
+
+  it 'display month name' do
     render
     expect(rendered).to match(/Janeiro/)
   end
 
   it 'displays the produces' do
-    assign(:item_season, item_season)
-    assign(
-      :item_season_produces,
-      ProduceItem.joins(:item_seasons).where(item_seasons: { month_index: item_season.month_index })
-    )
     render
     expect(rendered).to match(/Carambola/)
   end
