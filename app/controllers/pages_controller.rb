4a8dd78e895975559c238f4f8e4855173022bfa0
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-07-18 18:27:15 -0300

    moved setting the slug params to private so our params will be safer, and we have better security

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  before_action :set_slug, only: %i[show]
+
   def index; end
 
   def show
-    slug = params[:slug]
-    view_file = Rails.root.join('app', 'views', 'pages', "#{slug}.html.erb")
+    view_file = Rails.root.join('app', 'views', 'pages', "#{@slug}.html.erb")
 
     return render 'pages/404', status: :not_found unless view_file.exist?
 
     render "pages/#{slug}"
+  end
+
+  private
+
+  def set_slug
+    @slug = params[:slug]
   end
 end
