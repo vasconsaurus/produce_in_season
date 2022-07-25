0a3bab106a6239e9323bc412b043d47da8522f1b
Tales Cione <talesj@gmail.com>
2022-07-25 19:07:55 +0200

    [#19] Add annotate gem (#38)
    annotate is a gem that adds table schemas as comments at several files in our projects. This saves a trip to schema.rb whenever we want to check our table schemas.

--- spec/models/item_season_spec.rb
+++ spec/models/item_season_spec.rb
@@ -14,3 +14,25 @@ RSpec.describe ItemSeason, type: :model do
     it { is_expected.to belong_to(:produce_item).without_validating_presence }
   end
 end
+
+# rubocop:disable Layout/LineLength
+# == Schema Information
+#
+# Table name: item_seasons
+#
+#  id              :bigint           not null, primary key
+#  month_index     :integer          not null
+#  country_code    :string           not null
+#  produce_item_id :bigint           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_item_seasons_on_produce_item_id  (produce_item_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (produce_item_id => produce_items.id)
+#
+# rubocop:enable Layout/LineLength
