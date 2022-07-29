8de04ae458516704f1e5a00890942b915b486af7
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-07-29 11:13:22 -0300

    added a home_page_controller
    As suggested I generated a new controller just for the homepage and used
    a show action to display it. I'm keeping our pages controller for our extra
    stuff, like error messages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root to: 'pages#index'
+  root to: 'home_page#show'
 
   resources :produce_items, only: %i[index show]
   resources :item_seasons, only: %i[index show], param: :month_index
 
-  # get 'random/:id', to: 'songs#random', as: :random
   get ':slug', to: 'pages#show', as: 'pages'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
