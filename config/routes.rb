a5b896a3d3b758ed788fb0160ebc446e1fc78d55
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-08-08 13:14:47 -0300

    removed pages controller, views and tests After we discussed about the pages using slugs, I think it's best to keep it simple. From the start I should have only focused on the main page for this PR. So let's remove this for now, in another PR we will decide how to go about our errors, and if other extra pages are needed, we will discuss those separately.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :produce_items, only: %i[index show]
   resources :item_seasons, only: %i[index show], param: :month_index
 
-  get ':slug', to: 'pages#show', as: 'pages'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
