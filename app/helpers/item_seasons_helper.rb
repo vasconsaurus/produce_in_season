3403c773a339e84d3cc7f042303e3769bc7e34c2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-12 19:03:00 -0300

    added the fixed rotations
    I used the helper to rotate a certain amount based on a
    specific month index. This gives us always the same page
    layout, so it's more consistent. And it's probably faster.
    But is it the best solution?

--- app/helpers/item_seasons_helper.rb
+++ app/helpers/item_seasons_helper.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 module ItemSeasonsHelper
-  def randomized_rotation
-    %w[rotate-zero rotate-plus-5 rotate-plus-10 rotate-plus-15
-       rotate-minus-5 rotate-minus-10 rotate-minus-15].sample
+  def rotation(month_index)
+    case month_index
+    when 1, 6
+      'rotate-plus-5'
+    when 5
+      'rotate-minus-5'
+    when 12
+      'rotate-plus-10'
+    when 4, 9
+      'rotate-minus-10'
+    else
+      'rotate-zero'
+    end
   end
 end
--- spec/helpers/item_seasons_helper_spec.rb
+++ spec/helpers/item_seasons_helper_spec.rb
@@ -3,11 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe ItemSeasonsHelper, type: :helper do
-  describe 'randomized rotation' do
-    it 'adds random rotation to 12 items' do
-      array1 = Array.new(12) { helper.randomized_rotation }
-      array2 = Array.new(12) { helper.randomized_rotation }
-      expect(array1).not_to match(array2)
+  describe 'rotation' do
+    it 'adds rotation to month with index 12' do
+      expect(helper.rotation(12)).to eq('rotate-plus-10')
     end
   end
 end
