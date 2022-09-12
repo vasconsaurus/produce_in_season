5bf7b33cfdf0ab3c9ef358505db46f5c400522a4
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-12 11:35:21 -0300

    cleaned rubocop requests

--- app/helpers/item_seasons_helper.rb
+++ app/helpers/item_seasons_helper.rb
@@ -2,7 +2,7 @@
 
 module ItemSeasonsHelper
   def sampled_rotation
-    ["rotate-zero", "rotate-plus-5", "rotate-plus-10", "rotate-plus-15",
-      "rotate-minus-5", "rotate-minus-10", "rotate-minus-15"].sample
+    %w[rotate-zero rotate-plus-5 rotate-plus-10 rotate-plus-15
+       rotate-minus-5 rotate-minus-10 rotate-minus-15].sample
   end
 end
