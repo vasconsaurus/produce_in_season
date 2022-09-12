cf2fde00ed2f8f72082490fe7bf1569833d5da59
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-12 17:39:51 -0300

    updated tests per Tales' requests

--- spec/helpers/item_seasons_helper_spec.rb
+++ spec/helpers/item_seasons_helper_spec.rb
@@ -4,8 +4,10 @@ require 'rails_helper'
 
 RSpec.describe ItemSeasonsHelper, type: :helper do
   describe 'randomized rotation' do
-    it 'adds a random rotation to a button' do
-      expect(helper.randomized_rotation).to include('rotate')
+    it 'adds random rotation to 12 items' do
+      array1 = Array.new(12) { helper.randomized_rotation }
+      array2 = Array.new(12) { helper.randomized_rotation }
+      expect(array1).not_to match(array2)
     end
   end
 end
