473ef77fd007557bb019dcaa6ae1fac6b9232bac
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-12 11:55:29 -0300

    changed sampled_rotation to randomized_rotation
    Even though I'm using sample inside the helper, I think
    randomized_rotation makes what the helper is doing clearer.

--- app/helpers/item_seasons_helper.rb
+++ app/helpers/item_seasons_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ItemSeasonsHelper
-  def sampled_rotation
+  def randomized_rotation
     %w[rotate-zero rotate-plus-5 rotate-plus-10 rotate-plus-15
        rotate-minus-5 rotate-minus-10 rotate-minus-15].sample
   end
