bd6b4dc8777e92d7fae11583dccbc3c23548e73c
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-14 11:41:51 -0300

    cleaned up some text

--- spec/helpers/item_seasons_helper_spec.rb
+++ spec/helpers/item_seasons_helper_spec.rb
@@ -25,14 +25,3 @@ RSpec.describe ItemSeasonsHelper, type: :helper do
     end
   end
 end
-
-# when 1, 6
-#   'rotate-plus-5'
-# when 5
-#   'rotate-minus-5'
-# when 12
-#   'rotate-plus-10'
-# when 4, 9
-#   'rotate-minus-10'
-# else
-#   'rotate-0'
