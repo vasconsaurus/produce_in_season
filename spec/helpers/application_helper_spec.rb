f4c63866fc2f540dbe520b94949cca0cb3882aa2
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-09-14 11:20:41 -0300

    changed helper tests descriptions
    Since we are describing the method. In Ruby notation, # indicates
    instance methods and . or :: indicates class methods

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'formatted month name' do
+  describe '#formatted_month_name' do
     it 'changes the month_index to a month_name' do
       expect(helper.formatted_month_name(1)).to eq('janeiro')
     end
   end
 
-  describe 'formatted month short' do
+  describe '#formatted_month_short' do
     it 'changes the month_index to an abbr. month_name' do
       expect(helper.formatted_month_short(1)).to eq('jan')
     end
