5e0cb9c5b250ea8b58a99876beb57152b394ca56
Manu Vasconcelos <87862340+vasconsaurus@users.noreply.github.com>
2022-09-19 12:54:31 -0300

    Update app/helpers/item_seasons_helper.rb
    changed to use guard clause
    
    Co-authored-by: Tales Cione <talesj@gmail.com>

--- app/helpers/item_seasons_helper.rb
+++ app/helpers/item_seasons_helper.rb
@@ -2,6 +2,8 @@
 
 module ItemSeasonsHelper
   def rotation(month_index)
+    raise 'month-index must be between 1 and 12' unless 1..12.include?(month_index)
+    
     case month_index
     when 1, 6
       'rotate-plus-5'
@@ -11,10 +13,8 @@ module ItemSeasonsHelper
       'rotate-plus-10'
     when 4, 9
       'rotate-minus-10'
-    when 2, 3, 7, 8, 10, 11
-      'rotate-0'
     else
-      raise 'month-index must be between 1 and 12'
+      'rotate-0'
     end
   end
 end
