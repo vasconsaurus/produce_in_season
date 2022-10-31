f4c7b0527b7a16971170a62f72bf450466e0534e
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-31 14:05:25 -0300

    rewrote helper to make it deterministic

--- app/helpers/produce_items_helper.rb
+++ app/helpers/produce_items_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module ProduceItemsHelper
-  def formatted_month_list(produce, item_season)
-    produce.item_seasons.last.month_index == item_season.month_index ? formatted_month_name(item_season.month_index) : "#{formatted_month_name(item_season.month_index)} /"
+  def formatted_month_list(produce)
+    produce.item_seasons.order(month_index: :asc).map { formatted_month_name(_1.month_index) }.join(' / ')
   end
 
 end
