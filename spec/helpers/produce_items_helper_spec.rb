155813f6faed50b45b4fd697a6ae5fb188f7cf05
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-10-31 15:50:49 -0300

    helper test works, but without factories

--- spec/helpers/produce_items_helper_spec.rb
+++ spec/helpers/produce_items_helper_spec.rb
@@ -4,14 +4,16 @@ require 'rails_helper'
 
 RSpec.describe ProduceItemsHelper, type: :helper do
   context 'when viewing the produce items index' do
-    let(:produce_item) { ProduceItem.create(name: 'carambola', category: 'fruit') }
-    let(:item_season_jan) { ItemSeason.create(produce_item_id: produce_item.id, month_index: 1, country_code: 'BR') }
-    let(:item_season_fev) { ItemSeason.create(produce_item_id: produce_item.id, month_index: 2, country_code: 'BR') }
-
-    let(:produce_item_two) { ProduceItem.create(name: 'banana', category: 'fruit') }
-    let(:item_season_mar) { ItemSeason.create(produce_item_id: produce_item_two.id, month_index: 3, country_code: 'BR') }
+    let!(:produce_item) { ProduceItem.create(name: 'caqui', category: 'fruit') }
+    let!(:produce_item_two) { ProduceItem.create(name: 'quincam', category: 'fruit') }
 
     describe '#formatted_month_name' do
+      before do
+        ItemSeason.create(produce_item_id: produce_item.id, month_index: 1, country_code: 'BR')
+        ItemSeason.create(produce_item_id: produce_item.id, month_index: 2, country_code: 'BR')
+        ItemSeason.create(produce_item_id: produce_item_two.id, month_index: 3, country_code: 'BR')
+      end
+
       it 'adds / to the string, if its not the last or only month' do
         expect(helper.formatted_month_list(produce_item)).to eq('janeiro / fevereiro')
       end
