16c5dec5851c3f6e92f831102dda7fc57ce0ab42
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-09 14:45:12 -0300

    changed test to use subject

--- spec/models/item_season_spec.rb
+++ spec/models/item_season_spec.rb
@@ -3,11 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe ItemSeason, type: :model do
-  let!(:produce_item) { create(:produce_item) }
-  let!(:item_season) { described_class.new(produce_item_id: produce_item.id, month_index: 1, country_code: 'BR') }
+  subject(:item_season) { create(:item_season, produce_item_id: produce_item.id, month_index: 1, country_code: 'BR') }
 
-  it 'is valid with valid attributes' do
-    expect(item_season).to be_valid
+  let(:produce_item) { create(:produce_item) }
+
+  describe 'is valid with valid attributes' do
+    it { is_expected.to be_valid }
   end
 
   describe 'Associations' do
