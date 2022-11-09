2bad7027dc557525d07c4b46b686e4ecebbd2226
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-11-09 14:28:54 -0300

    modified some tests to be more readable

--- spec/helpers/produce_items_helper_spec.rb
+++ spec/helpers/produce_items_helper_spec.rb
@@ -3,23 +3,36 @@
 require 'rails_helper'
 
 RSpec.describe ProduceItemsHelper, type: :helper do
-  context 'when viewing the produce items index' do
-    let!(:produce_item) { create(:produce_item) }
-    let!(:produce_item_two) { create(:produce_item) }
+  shared_context 'when viewing the produce items index' do
+    let!(:produce_item_multiple) { create(:produce_item) }
+    let!(:produce_item_one) { create(:produce_item) }
+    let(:produce_item_empty) { create(:produce_item) }
 
-    describe '#formatted_month_name' do
-      before do
-        create(:item_season, produce_item: produce_item, month_index: 1)
-        create(:item_season, produce_item: produce_item, month_index: 2)
-        create(:item_season, produce_item: produce_item_two, month_index: 3)
+    before do
+      create(:item_season, produce_item: produce_item_multiple, month_index: 1)
+      create(:item_season, produce_item: produce_item_multiple, month_index: 2)
+      create(:item_season, produce_item: produce_item_one, month_index: 3)
+    end
+  end
+
+  describe '#formatted_month_name' do
+    include_context 'when viewing the produce items index'
+
+    describe 'when produce item has multiple seasons' do
+      it 'formats months using / as a separator' do
+        expect(helper.formatted_month_list(produce_item_multiple)).to eq('janeiro / fevereiro')
       end
+    end
 
-      it 'adds / to the string, if its not the last or only month' do
-        expect(helper.formatted_month_list(produce_item)).to eq('janeiro / fevereiro')
+    describe 'when produce item has one season' do
+      it 'returns the month name' do
+        expect(helper.formatted_month_list(produce_item_one)).to eq('março')
       end
+    end
 
-      it 'does not add / to the string, if its the last or only month' do
-        expect(helper.formatted_month_list(produce_item_two)).to eq('março')
+    describe 'when produce item has no seasons' do
+      it 'returns an empty string' do
+        expect(helper.formatted_month_list(produce_item_empty)).to eq('')
       end
     end
   end
